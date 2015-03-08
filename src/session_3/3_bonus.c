ac1c67db429c686c2d57f712b854d662cf191cbe
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2015-03-08 02:26:33 +0100

    Finished bonus exercise

--- src/session_3/3_bonus.c
+++ src/session_3/3_bonus.c
@@ -1,14 +1,6 @@
-#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/param.h>
-
-#include <wiringPi.h>
-#include <softPwm.h>
-
-int *numbers;
 
 typedef struct listptr
 {
@@ -27,14 +19,17 @@ listptr* create_list(int len)
 
 void delete_list(listptr* list)
 {
-  free(list->start);
-  free(list);
+  if (list != NULL)
+  {
+    if (list->start != NULL)
+      free(list->start);
+    free(list);
+  }
 }
 
 void printlist(listptr* l)
 {
-  int i;
-  for (i = 0; i < l->len; i++)
+  for (int i = 0; i < l->len; i++)
   {
     printf("[%d] -> %d\n", i, (l->start)[i]);
   }
@@ -50,35 +45,31 @@ listptr* merge(listptr* l, listptr* r)
   while (left < l->len && right < r->len)
   {
     if ((l->start)[left] <= (r->start)[right])
-    {
       res->start[idx++] = (l->start)[left++];
-    }
     else
-    {
       res->start[idx++] = (r->start)[right++];
-    }
   }
-  while (left < l->len)
-  {
-    res->start[idx++] = (l->start)[left++];
-  }
-  while (right < r->len)
-  {
-    res->start[idx++] = (r->start)[right++];
-  }
+  while (left < l->len)  res->start[idx++] = (l->start)[left++];
+  while (right < r->len) res->start[idx++] = (r->start)[right++];
 
   return res;
 }
 
+void* mergefun(void *params)
+{
+  listptr* left  = ((listptr**)params)[0];
+  listptr* right = ((listptr**)params)[1];
+
+  return merge(left, right);
+}
+
 listptr* sort(listptr *list)
 {
-  if (list->len == 1)
-  {
-    return list;
-  }
+  if (list->len <= 1) return list;
 
-  listptr l     = { list->start, list->len / 2 };
-  listptr r     = { list->start + list->len / 2, list->len - list->len / 2 };
+  int mid       = list->len / 2;
+  listptr l     = { list->start, mid };
+  listptr r     = { list->start + mid, list->len - mid };
   listptr *lres = sort(&l);
   listptr *rres = sort(&r);
   listptr *res  = merge(lres, rres);
@@ -88,39 +79,70 @@ listptr* sort(listptr *list)
   return res;
 }
 
-void *sortfun(void *params)
+void* sortfun(void *params)
 {
   listptr* list = (listptr*)params;
   listptr* res  = sort(list);
-  printlist(res);
-  free(res);
 
-  return NULL;
+  return res;
 }
 
 int main(int argc, char **argv)
 {
-  int i;
+  pthread_t left_thread;
+  pthread_t right_thread;
+  pthread_t merge_thread;
+
+  listptr *leftres  = NULL;
+  listptr *rightres = NULL;
+  listptr *mergeres = NULL;
+
+  int ret = 0;
   int idx = argc - 1;
-  numbers = malloc(sizeof(int) * idx);
+  int *numbers = NULL;
 
   if (idx < 1) goto err;
-  for (i = 1; i <= idx; i++)
+
+  numbers = malloc(sizeof(int) * idx);
+  for (int i = 1; i <= idx; i++)
   {
     int n = strtol(argv[i], NULL, 10);
     if (n == 0 && *argv[i] != '0') goto err;
     numbers[i - 1] = n;
   }
 
-  pthread_t sort_thread;
-  listptr list = { numbers, idx };
-  if (pthread_create(&sort_thread, NULL, &sortfun, &list) != 0) goto err;
-  if (pthread_join(sort_thread, NULL) != 0)                     goto err;
-  return 0;
+  if (idx < 4)
+  {
+    listptr list  = { numbers, idx };
+    if (pthread_create(&left_thread,  NULL, &sortfun, &list)  != 0) goto err;
+    if (pthread_join  ( left_thread,  (void**)&mergeres)  != 0)     goto err;
+  }
+  else
+  {
+    listptr left  = { numbers, idx / 2 };
+    listptr right = { numbers + idx / 2, idx - idx / 2 };
+    if (pthread_create(&left_thread,  NULL, &sortfun, &left)  != 0) goto err;
+    if (pthread_create(&right_thread, NULL, &sortfun, &right) != 0) goto err;
+    if (pthread_join  ( left_thread,  (void**)&leftres)  != 0)      goto err;
+    if (pthread_join  ( right_thread, (void**)&rightres) != 0)      goto err;
+
+    listptr *mergeparams[] = { leftres, rightres };
+    if (pthread_create(&merge_thread, NULL, &mergefun, mergeparams ) != 0) goto err;
+    if (pthread_join  ( merge_thread, (void**)&mergeres) != 0)             goto err;
+  }
+
+  printlist(mergeres);
+  goto clean;
 
   err:
-  printf("Error!\n");
-  free(numbers);
-  return 1;
+  printf("Error!\n"); ret = 1;
+
+  clean:
+  if (leftres  != NULL) delete_list(leftres);
+  if (rightres != NULL) delete_list(rightres);
+  if (mergeres != NULL) delete_list(mergeres);
+  if (numbers  != NULL) free(numbers);
+
+  return ret;
 }
 
