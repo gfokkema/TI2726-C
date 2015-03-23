a64360f44ba9adb9cd2c611212c24e9121123a32
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2015-03-23 22:00:02 +0100

    Finished exercise 5.1-5.3

--- src/session_5/5_1.c
+++ src/session_5/5_1.c
@@ -14,8 +14,6 @@
 #define LED3 2
 #define LED4 3
 
-sem_t mutex;
-sem_t semaphore;
 int shared = 0;
 
 void init()
@@ -40,16 +38,15 @@ void *dispfun()
   init();
 
   while (1) {
-    sem_wait(&semaphore);       /** down semaphore : wait until data is produced */
-    sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
     int n = shared;
     printf("dispthread\t%lu: %d\n", pthread_self(), n);
-    sem_post(&mutex);           /** up semaphore : exit mutual exclusion */
 
     digitalWrite(LED1, (n >> 0) & 1);
     digitalWrite(LED2, (n >> 1) & 1);
     digitalWrite(LED3, (n >> 2) & 1);
     digitalWrite(LED4, (n >> 3) & 1);
+
+    sleep(1);
   }
 
   reset();
@@ -59,13 +56,10 @@ void *dispfun()
 void *incfun()
 {
   int i;
-  for (i = 0; i <= 15; i++)
+  for (i = 0; i < 15; i++)
   {
-    sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
     shared++;
     printf("incthread\t%lu\n", pthread_self());
-    sem_post(&mutex);           /**   up semaphore : exit mutual exclusion */
-    sem_post(&semaphore);       /**   up semaphore : data was produced */
 
     sleep(1);
   }
@@ -76,13 +70,10 @@ void *incfun()
 void *decfun()
 {
   int i;
-  for (i = 0; i <= 10; i++)
+  for (i = 0; i < 10; i++)
   {
-    sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
     shared--;
     printf("decthread\t%lu\n", pthread_self());
-    sem_post(&mutex);           /** up semaphore : exit mutual exclusion */
-    sem_post(&semaphore);       /**   up semaphore : data was produced */
 
     sleep(1);
   }
@@ -113,9 +104,6 @@ int join(pthread_t thread, char* message)
 
 int main()
 {
-  sem_init(&mutex,     0, 1);
-  sem_init(&semaphore, 0, 0);
-
   pthread_t      inc_thread;
   pthread_t      dec_thread;
   pthread_t      disp_thread;
@@ -124,18 +112,15 @@ int main()
   struct sched_param high = { 99 };
 
   create(&inc_thread,  NULL, &incfun,  "inc_thread");
-  pthread_setschedparam(inc_thread,  SCHED_RR, &low);
+  pthread_setschedparam(inc_thread,  SCHED_RR, &high);
   create(&dec_thread,  NULL, &decfun,  "dec_thread");
   pthread_setschedparam(dec_thread,  SCHED_RR, &low);
   create(&disp_thread, NULL, &dispfun, "disp_thread");
-  pthread_setschedparam(disp_thread, SCHED_RR, &high);
+  pthread_setschedparam(disp_thread, SCHED_RR, &low);
 
   join(inc_thread,  "inc_thread");
   join(dec_thread,  "dec thread");
-  join(disp_thread, "disp thread");
-
-  sem_destroy(&semaphore);
-  sem_destroy(&mutex);
+  pthread_cancel(disp_thread);
 
   return 0;
 }
--- src/session_5/5_2.c
+++ src/session_5/5_2.c
@@ -15,8 +15,6 @@
 #define LED4 3
 
 sem_t mutex;
-sem_t decmutex;
-sem_t semaphore;
 int shared = 0;
 
 void init()
@@ -41,16 +39,15 @@ void *dispfun()
   init();
 
   while (1) {
-    sem_wait(&semaphore);       /** down semaphore : wait until data is produced */
-    sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
     int n = shared;
     printf("dispthread\t%lu: %d\n", pthread_self(), n);
-    sem_post(&mutex);           /** up semaphore : exit mutual exclusion */
 
     digitalWrite(LED1, (n >> 0) & 1);
     digitalWrite(LED2, (n >> 1) & 1);
     digitalWrite(LED3, (n >> 2) & 1);
     digitalWrite(LED4, (n >> 3) & 1);
+
+    sleep(1);
   }
 
   reset();
@@ -59,38 +56,35 @@ void *dispfun()
 
 void *incfun()
 {
+  sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
+
   int i;
-  for (i = 0; i <= 15; i++)
+  for (i = 0; i < 15; i++)
   {
-    sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
     shared++;
     printf("incthread\t%lu\n", pthread_self());
-    sem_post(&mutex);           /**   up semaphore : exit mutual exclusion */
-    sem_post(&semaphore);       /**   up semaphore : data was produced */
 
     sleep(1);
   }
 
-  sem_post(&decmutex);
+  sem_post(&mutex);           /**   up semaphore : exit mutual exclusion */
   return NULL;
 }
 
 void *decfun()
 {
-  sem_wait(&decmutex);
+  sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
 
   int i;
-  for (i = 0; i <= 10; i++)
+  for (i = 0; i < 10; i++)
   {
-    sem_wait(&mutex);           /** down semaphore : enter mutual exclusion */
     shared--;
     printf("decthread\t%lu\n", pthread_self());
-    sem_post(&mutex);           /** up semaphore : exit mutual exclusion */
-    sem_post(&semaphore);       /**   up semaphore : data was produced */
 
     sleep(1);
   }
 
+  sem_post(&mutex);           /**   up semaphore : exit mutual exclusion */
   return NULL;
 }
 
@@ -118,8 +112,6 @@ int join(pthread_t thread, char* message)
 int main()
 {
   sem_init(&mutex,     0, 1);
-  sem_init(&decmutex,  0, 0);
-  sem_init(&semaphore, 0, 0);
 
   pthread_t      inc_thread;
   pthread_t      dec_thread;
@@ -129,17 +121,16 @@ int main()
   struct sched_param high = { 99 };
 
   create(&inc_thread,  NULL, &incfun,  "inc_thread");
-  pthread_setschedparam(inc_thread,  SCHED_RR, &low);
+  pthread_setschedparam(inc_thread,  SCHED_RR, &high);
   create(&dec_thread,  NULL, &decfun,  "dec_thread");
   pthread_setschedparam(dec_thread,  SCHED_RR, &low);
   create(&disp_thread, NULL, &dispfun, "disp_thread");
-  pthread_setschedparam(disp_thread, SCHED_RR, &high);
+  pthread_setschedparam(disp_thread, SCHED_RR, &low);
 
   join(inc_thread,  "inc_thread");
   join(dec_thread,  "dec thread");
-  join(disp_thread, "disp thread");
+  pthread_cancel(disp_thread);
 
-  sem_destroy(&semaphore);
   sem_destroy(&mutex);
 
   return 0;
